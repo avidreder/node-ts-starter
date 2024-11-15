9970a6c9bb0a38958f36ced0789e7a3e7fdcd789
Andrew Reder <avidreder@gmail.com>
2024-11-15 09:21:50 -0800

    more setup

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@
 // executable package. E.g. code that can be used as cli like prettier or eslint
 
 const main = () => {
+	for (let arg of process.argv) {
+		console.log({ arg })
+	}
 	console.log("hello Node.js and Typescript world :]");
-	console.log("live reloading");
-	console.log("live reloading2");
 };
 
 // This was just here to force a linting error for now to demonstrate/test the
